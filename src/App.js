4e72f30fe8a2489b73f1ac7b050c5c837c03e188
Abdullah Mashaan <eng.aalenezi@gmail.com>
2024-11-10 20:22:35 +0300

    Task one Solved

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import storeImage from "./levain bakery.jpeg";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <h1> Levain Bakery</h1>
+      <p> New York City's Most Famous Cookies</p>
+      <img className="Store-image" src={storeImage}></img>
+
     </div>
   );
 }

4cbb6c178a0f294797c457a30f04bbe2bddfaf9b
Abdullah Mashaan <eng.aalenezi@gmail.com>
2024-11-11 18:19:07 +0300

    Task 1,2&3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import storeImage from "./levain bakery.jpeg";
-
+import products from './products';
 
 function App() {
   return (
@@ -9,8 +9,20 @@ function App() {
       <p> New York City's Most Famous Cookies</p>
       <img className="Store-image" src={storeImage}></img>
 
+
+
+ <div className="product-container">
+        {products.map((product, index) => (
+          <div key={index} className="product-card">
+            <img src={product.image} alt={product.name} />
+            <h2>{product.name}</h2>
+            <p>{product.price}</p>
+          </div>
+        ))}
+
+      </div>
     </div>
   );
 }
-
 export default App;
+
---
+++ src/products.js
@@ -0,0 +1,29 @@
+import imgChocolateChipCookie from "./chocolateChipWalnut.jpeg"
+import imgDarkChoclateCookie from "./darkChocolateChip.jpg"
+
+const products = [
+    {
+      name: "Chocolate Chip Walnut Cookie",
+      price: "1 KD",
+      image: imgChocolateChipCookie,
+    },
+    {
+      name: "Dark Chocolate Cookie",
+      price: "1 KD",
+      image: imgDarkChoclateCookie, 
+    },
+    {
+      name: "Dark Chocolate Cookie",
+      price: "1 KD",
+      image: imgDarkChoclateCookie, 
+    },
+    {
+      name: "Dark Chocolate Cookie",
+      price: "1 KD",
+      image: imgDarkChoclateCookie, 
+    }
+  ];
+  
+  export default products;
+
+  
